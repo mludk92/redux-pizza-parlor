fbe39a679d2b03ee63d70dc31241cd3f376c31d3
Mitch Ludkey <mludk92@hotmail.com>
2023-04-08 09:04:34 -0500

    Index and task set up

--- src/index.js
+++ src/index.js
@@ -2,10 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { createStore ,combineReducers, applyMiddleware} from 'redux'
+import { Provider } from 'react-redux';
+import logger from 'redux-logger'
+
+//Reducers store data
+
+const orderId = (state = 0, action)=>{
+    console.log(`I'm a reducer!`)
+    if(action.type === 'INCREASE_ORDER_ID'){
+    //If not change return existing value
+    //! Never set state equal to anything (e.g. DO NOT state++)
+    return state + 1;
+}
+    return state 
+}
+
+/** TODO: Create store */
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+            orderId,
+            //OTHER REDUCERS ADDED HERE
+        }
+    ),
+    //Set up logger
+    applyMiddleware(logger)
+
+)
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={storeInstance}>
     <React.StrictMode>
         <App />
     </React.StrictMode>
+    </Provider>
 );
