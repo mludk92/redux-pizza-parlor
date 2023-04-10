73d7889526b0eaa6bfa2d9d810b7d117741c0959
mludkey <mitchell.ludkey@blueearthcountymn.gov>
2023-04-10 15:49:59 -0500

    set up form to useState defaults values set for inputs

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -1,24 +1,34 @@
-import './OrderDetails.css'
+import { useState } from 'react';
+import './OrderDetails.css';
 
 function OrderDetails() {
+  const [name, setName] = useState('Name');
+  const [city, setCity] = useState('City');
+  const [zipCode, setZipCode] = useState('Zip Code');
+  const [streetAddress, setStreetAddress] = useState('Street Address');
+  const [deliveryOption, setDeliveryOption] = useState('Pick Up');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission here
+  }
+
   return (
     <div> 
-    <form class="form-style-9">
+    <form class="form-style-9" onSubmit={handleSubmit}>
         <ul>
         <li>
-            <input type="text" name="field1" class="field-style field-full align-none" placeholder="Name" />
-            
-
+            <input type="text" name="field1" class="field-style field-full align-none" placeholder={name} onChange={(e) => setName(e.target.value)} />
         </li>
         <li>
-            <input type="text" name="field3" class="field-style field-split align-left" placeholder="City" />
-            <input type="number" name="field4" class="field-style field-split align-right" placeholder="Zip Code" />
+            <input type="text" name="field3" class="field-style field-split align-left" placeholder={city} onChange={(e) => setCity(e.target.value)} />
+            <input type="number" name="field4" class="field-style field-split align-right" placeholder={zipCode} onChange={(e) => setZipCode(e.target.value)} />
         </li>
         <li>
-        <input type="text" name="field5" class="field-style field-full align-none" placeholder="Street Address" />
+        <input type="text" name="field5" class="field-style field-full align-none" placeholder={streetAddress} onChange={(e) => setStreetAddress(e.target.value)} />
         </li>
         <li>
-        <select type="text" name="field6" >
+        <select type="text" name="field6" value={deliveryOption} onChange={(e) => setDeliveryOption(e.target.value)}>
         <option value="Pick Up">PickUp</option>
         <option value="Delivery">Delivery</option>
         </select>
